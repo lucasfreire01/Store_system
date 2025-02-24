9cefe563e3e7ebea5125be3edc3398b73a897383
Lucas Gabriel Soares Freire <ls882674@gmail.com>
2025-02-24 11:19:09 -0300

    backend

---
+++ backend.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
+import random as rd
+import datetime
+import string
+
+
+con = sqlite3.connect("database.db")
+class backend:
+    
