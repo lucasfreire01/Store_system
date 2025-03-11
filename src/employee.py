0d2553d78582deaf67c5ca92a918e3b785226888
Lucas Gabriel Soares Freire <ls882674@gmail.com>
2025-03-11 10:31:35 -0300

    employee commit

--- src/employee.py
+++ src/employee.py
@@ -1,13 +1,17 @@
 from .user import User
+from .base import DatabaseInventory
 
 class Employee(User):
 
 
-    def __init__(self, position: str ,args*):
+    db = DatabaseInventory()
+    def __init__(self, position: str ,*args):
 
-        super().__init__(args*)
+        db = DatabaseInventory()
+        super().__init__(*args)
         employee_id = self.get_employee_id()
+        self.db = DatabaseInventory()
         position = self.position
 
-    def get_employee_id(self):
-        pass
+    def get_employee_id(self,  id: None):
+        load_id = self.db.execute("""SELECT * FROM suppliers WHERE id_supplier = ?""", args=(id,))
